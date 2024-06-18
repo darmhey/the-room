15548f86cbf3278b9c9665f1e2d8c4bb0efe7bca
Damilola <johnasco19@gmail.com>
2024-06-18 13:18:13 +0100

    signIn and signOut actions

--- app/account/page.js
+++ app/account/page.js
@@ -1,6 +1,9 @@
+import { auth } from "../_lib/auth";
 export const metadata = {
   title: "Your Account",
 };
-export default function Page() {
-  return <h1 className=" text-accent-400">Welcome, Damilola </h1>;
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+  return <h1 className=" text-accent-400">Welcome, {firstName} </h1>;
 }
