2c18b5c5043c07ea60db1a042aecd70c9b721508
Damilola <johnasco19@gmail.com>
2024-06-18 12:36:42 +0100

    middleware for account route

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -9,7 +9,7 @@ function ReservationForm({ cabin, user }) {
   return (
     <div className="scale-[1.01]">
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
-        <p>Logged in as {user ? user.name : " "} </p>
+        <p>Logged in as: {user ? user.name : " "} </p>
 
         {/* <div className='flex gap-4 items-center'>
           <img
