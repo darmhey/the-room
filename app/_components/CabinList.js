8aad5a4a381e4a8043f4af329987caadf7629e5a
Damilola <johnasco19@gmail.com>
2024-08-03 22:17:28 +0100

    made cabin page responsive

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -13,11 +13,12 @@ async function CabinList({ filter }) {
     displayedCabins = cabins.filter(
       (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
     );
+
   if (filter === "large")
     displayedCabins = cabins.filter((cabin) => cabin.maxCapacity > 8);
 
   return (
-    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
       {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
